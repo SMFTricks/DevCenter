8a40fd8d44b81f883d37c3a6e1c71ccb960b7432
Diego Andrés <diegoandres_cortes@outlook.com>
2022-03-20 17:35:56 -0600

    add template
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

---
+++ Themes/default/DevCenter.template.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @package Dev Center
+ * @version 1.0
+ * @author Diego Andrés <diegoandres_cortes@outlook.com>
+ * @copyright Copyright (c) 2022, SMF Tricks
+ * @license https://www.gnu.org/licenses/gpl-3.0.en.html
+ */
+
+function template_devcenter_above() {}
+
+/* display the average server load */
+function template_devcenter_below()
+{
+	global $txt, $context;
+
+	if (!empty($context['devcenter_serverload']))
+		echo '
+		<p>
+			', sprintf($txt['devcenter_load'], $context['devcenter_serverload'][0], $context['devcenter_serverload'][1], $context['devcenter_serverload'][2]), '
+		</p>';
+}
