ebd6934ca3c16849d4329449539b130d65768572
Diego Andrés <diegoandres_cortes@outlook.com>
2022-03-20 17:35:40 -0600

    update language
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

--- Themes/default/languages/DevCenter/.english.php
+++ Themes/default/languages/DevCenter/.english.php
@@ -8,13 +8,14 @@
  * @license https://www.gnu.org/licenses/gpl-3.0.en.html
  */
 
-// DevCenter mod -- Settings
-$txt['mods_cat_modifications_devcenter'] = 'DevCenter settings';
-$txt['devcenter'] = 'DevCenter settings';
+global $scripturl;
 
-$txt['devcenter_menu_count_log_entries'] = 'Show the amount of error log entries in the Admin menu button';
-$txt['devcenter_dont_show_when_0'] = 'Do not show the counter when there are no entries';
+// DevCenter mod -- Settings
+$txt['devcenter'] = 'DevCenter settings';
+$txt['devcenter_desc'] = 'Here you can configure the DevCenter mod.';
+
 $txt['devcenter_direct_printing_error'] = 'Show debugging information (including errors, will be shown to every user!)';
 $txt['devcenter_show_phpinfo'] = 'Show phpinfo() when action \'phpinfo\' is called';
+$txt['devcenter_show_phpinfo_desc'] = 'Only admins can access and see it in the <a href="' . $scripturl . '?action=phpinfo">phpinfo()</a> page.';
 $txt['devcenter_displayserverload'] = 'Display the server load in the footer <strong>(not supported on Windows hosts!)</strong>';
 $txt['devcenter_load'] = 'Server load over the past 5, 10 and 15 minutes respectively: %s, %s, %s';
